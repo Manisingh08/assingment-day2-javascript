e467c4d48ca747c6e1bae1e3c506089d8fcb694d
Mani <68222013+Manisingh08@users.noreply.github.com>
2020-07-16 13:15:53 +0530

    Add files via upload

---
+++ assingment3/variable1.js
@@ -0,0 +1,6 @@
+var x = 16 + "Volvo";
+//var x = "16" + "rahul";    Volvo16
+//var x = 16 + "rakesh";      16Volvo
+
+var x = 16 + 4 + "ramesh";
+document.getElementById("demo").innerHTML = x
---
+++ assingment3/variable2.js
@@ -0,0 +1,8 @@
+ 
+
+var carName1 = "hundai";
+var carName2 = 'honda';
+
+document.getElementById("demo").innerHTML =
+carName1 + "<br>" + 
+carName2;
